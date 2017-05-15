5c8d6150c266b0e767ecf1f62d38d7a7a168ed58
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-05-15 18:29:26 -0400

    Colocando primeras validaciones básicas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var name = document.getElementById("name").value;
+	var lastname = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var password = document.getElementById("input-password").value;
+	var selector = document.getElementsByClassName("form-control")[4].value;
+	var twitter = document.getElementById("input-social").value;
 }
