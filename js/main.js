6fa0c7a3bb112c1ed8757bc1875379996d3a082a
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-05-16 07:49:32 -0400

    Validaciones de campo vacío

--- js/main.js
+++ js/main.js
@@ -5,4 +5,16 @@ function validateForm(){
 	var password = document.getElementById("input-password").value;
 	var selector = document.getElementsByClassName("form-control")[4].value;
 	var twitter = document.getElementById("input-social").value;
+
+
+////para resetaer los campos y dejarlos nuevamente en blanco
+	document.getElementById("name").value = "";
+	document.getElementById("lastname").value = "";
+	document.getElementById("input-email").value = "";
+	document.getElementById("input-password").value = "";
+	document.getElementById("select").value = "0";
+	document.getElementById("input-social").value = "";
+	document.getElementById("el-checkbox").checked = "";	
 }
+
+	
